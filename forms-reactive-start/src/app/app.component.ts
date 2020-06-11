f37682ebee3d17fb2af5fdfbf99ef395acc706b6
alvalle09 <phredvalle@gmail.com>
2020-06-11 01:23:43 -0500

    use setValue and patchValue for setting default values

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -31,10 +31,25 @@ export class AppComponent implements OnInit{
     this.signupForm.statusChanges.subscribe(
       (status) => console.log(status)
     );
+
+    this.signupForm.setValue({
+      'userData': {
+        'username': 'Fred',
+        'email': 'fred@test.com'
+      },
+      'gender': 'male',
+      'hobbies': []
+    })
+
+    this.signupForm.patchValue({
+      'userData': {
+        'username': 'new name',
+      }    })
   }
 
   onSubmit() {
     console.log(this.signupForm);
+    this.signupForm.reset();
   }
 
   onAddHobby() {
